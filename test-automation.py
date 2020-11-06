0ab713838b249959e98271d5300a46552b505988
Adiel Pereira <adielp16@gmail.com>
2020-11-06 00:57:35 -0300

    desafio 3 concluido!

---
+++ test-automation.py
@@ -0,0 +1,12 @@
+import rpa as r
+
+r.init(visual_automation = True)
+r.url("http://google.com")
+r.type(600, 300, 'open source')
+r.click(900, 300)
+r.snap('page.bmp', 'results.bmp')
+r.hover('button_to_drag.bmp')
+r.mouse('down')
+r.hover(r.mouse_x() + 300, r.mouse_y())
+r.mouse('up')
+r.close()
